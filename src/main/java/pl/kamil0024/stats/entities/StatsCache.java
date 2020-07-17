426ed3e818a4a097e6bab46f157d3186ad266f51
KAMIL0024 <kamilbydpol@gmail.com>
2020-07-17 22:21:12 +0200

    Statystyki

--- src/main/java/pl/kamil0024/stats/entities/StatsCache.java
+++ src/main/java/pl/kamil0024/stats/entities/StatsCache.java
@@ -1,5 +1,6 @@
 package pl.kamil0024.stats.entities;
 
+import com.google.gson.Gson;
 import lombok.Data;
 import pl.kamil0024.bdate.BDate;
 import pl.kamil0024.core.database.StatsDao;
@@ -29,12 +30,13 @@ public class StatsCache {
 
     public void add(String id, Statystyka stats) {
         Statystyka statystyka = getStatystykaMap().getOrDefault(id, new Statystyka());
+        Log.debug(new Gson().toJson(statystyka));
 
         statystyka.setNapisanychWiadomosci(stats.getNapisanychWiadomosci() + statystyka.getNapisanychWiadomosci());
         statystyka.setUsunietychWiadomosci(stats.getUsunietychWiadomosci() + statystyka.getUsunietychWiadomosci());
         statystyka.setZbanowanych(stats.getZbanowanych() + statystyka.getZbanowanych());
         statystyka.setZmutowanych(stats.getZmutowanych() + statystyka.getZmutowanych());
-
+        Log.debug(new Gson().toJson(statystyka));
         save(id, stats);
     }
 
