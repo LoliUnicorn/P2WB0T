7edc030608cbf4bd43d0463bc2fe4bdce01fef19
KAMIL0024 <kamilbydpol@gmail.com>
2020-07-18 02:05:28 +0200

    Statystyki

--- src/main/java/pl/kamil0024/stats/entities/StatsCache.java
+++ src/main/java/pl/kamil0024/stats/entities/StatsCache.java
@@ -43,7 +43,6 @@ public class StatsCache {
     public synchronized void databaseSave() {
         for (Map.Entry<String, Statystyka> entry : getStatystykaMap().entrySet()) {
             StatsConfig sc = statsDao.get(entry.getKey());
-            Log.debug("tak " + entry.getValue().getDay());
             Statystyka dzisiaj = StatsConfig.getStatsFromDay(sc.getStats(), entry.getValue().getDay());
             if (dzisiaj == null) {
                 Log.debug("dzisiaj jest nullem");
