525345628b370f7439b1b6a819839951394362ac
KAMIL0024 <kamilbydpol@gmail.com>
2020-07-25 16:28:50 +0200

    API w fazie alpha v0.1.0

--- src/main/java/pl/kamil0024/core/database/config/UserConfig.java
+++ src/main/java/pl/kamil0024/core/database/config/UserConfig.java
@@ -1,16 +1,14 @@
 package pl.kamil0024.core.database.config;
 
-import gg.amy.pgorm.annotations.PrimaryKey;
 import lombok.Data;
 
 @Data
 public class UserConfig {
-    public UserConfig() { }
 
     public UserConfig(String id) {
         this.id = id;
     }
-    
+
     private String id = "";
 
     private String mcNick = null;
