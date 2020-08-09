d6e3d91273de173f3acfcac4ec136fb401a12277
KAMIL0024 <kamilbydpol@gmail.com>
2020-08-09 17:23:22 +0200

    Pierwsza alpha v1.0

--- musicbot/src/main/java/pl/kamil0024/musicbot/api/handlers/ShutdownHandler.java
+++ musicbot/src/main/java/pl/kamil0024/musicbot/api/handlers/ShutdownHandler.java
@@ -2,6 +2,8 @@ package pl.kamil0024.musicbot.api.handlers;
 
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.managers.AudioManager;
 import net.dv8tion.jda.api.sharding.ShardManager;
 import pl.kamil0024.musicbot.api.Response;
 
@@ -20,7 +22,14 @@ public class ShutdownHandler implements HttpHandler {
         } catch (Exception e) {
             Response.sendErrorResponse(ex, "Błąd", "Wystąpił błąd: " + e.getLocalizedMessage());
         }
-        new Thread(() -> System.exit(0)).start();
+        new Thread(() -> {
+            Guild guild = Connect.getGuild(api);
+            AudioManager state = guild.getAudioManager();
+            if (state.getConnectedChannel() != null) {
+                state.closeAudioConnection();
+            }
+            System.exit(0);
+        }).start();
 
     }
 
