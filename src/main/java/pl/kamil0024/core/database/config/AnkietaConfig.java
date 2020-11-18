99bdd77d716da441a21ef911b8147817ed7f028a
KAMIL0024 <kamilbydpol@gmail.com>
2020-11-18 02:40:34 +0100

    Ok

--- src/main/java/pl/kamil0024/core/database/config/AnkietaConfig.java
+++ src/main/java/pl/kamil0024/core/database/config/AnkietaConfig.java
@@ -22,7 +22,6 @@ package pl.kamil0024.core.database.config;
 import gg.amy.pgorm.annotations.GIndex;
 import gg.amy.pgorm.annotations.PrimaryKey;
 import gg.amy.pgorm.annotations.Table;
-import jdk.internal.org.objectweb.asm.Handle;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
