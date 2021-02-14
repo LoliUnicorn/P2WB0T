4baf02e3e6569a098d2c8b9f620195450a40a090
KAMIL0024 <kamilbydpol@gmail.com>
2021-02-14 14:11:58 +0100

    Jakies zmiany

--- src/main/java/gg/amy/pgorm/PgMapper.java
+++ src/main/java/gg/amy/pgorm/PgMapper.java
@@ -558,9 +558,9 @@ public class PgMapper<T> {
 
     public List<T> getAllTicketsByFiltr(int offset, String admId, boolean read) {
         final List<T> data = new ArrayList<>();
-        String msg = String.format("SELECT * FROM %s WHERE NOT(data::jsonb @> '{\"ocena\": -1}') AND data::jsonb @> '{\"spam\": false}' AND (data->>'readBy')::jsonb ?? '%s' ORDER BY data->>'createdTime' DESC LIMIT 10 OFFSET %d;", table.value(), admId, offset);
+        String msg = String.format("SELECT * FROM %s WHERE NOT(data::jsonb @> '{\"ocena\": -1}') AND data::jsonb @> '{\"spam\": false}' AND (data->>'readBy')::jsonb ?? '%s' ORDER BY data->>'createdTime';", table.value(), admId);
         if (!read) {
-            msg = String.format("SELECT * FROM %s WHERE NOT(data::jsonb @> '{\"ocena\": -1}') AND data::jsonb @> '{\"spam\": false}' AND NOT((data->>'readBy')::jsonb ?? '%s') ORDER BY data->>'createdTime' DESC LIMIT 10 OFFSET %d;", table.value(), admId, offset);
+            msg = String.format("SELECT * FROM %s WHERE NOT(data::jsonb @> '{\"ocena\": -1}') AND data::jsonb @> '{\"spam\": false}' AND NOT((data->>'readBy')::jsonb ?? '%s') ORDER BY data->>'createdTime';", table.value(), admId);
         }
         store.sql(msg, c -> {
             final ResultSet resultSet = c.executeQuery();
